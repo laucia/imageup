ddadfac0ad21299d17068453a8824ff29fe11918
Lauris Jullien <lauris.jullien@telecom-paristech.org>
2014-01-21 18:26:21 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
  
 setup(
     name='imageup',
-    version='0.0.1',
+    version='0.0.2',
     description='Basic Django admin image uploader',
     author='Lauris Jullien',
     author_email='lauris@captain-startup.com',
